30a758b487f78da2c35b0e19f3abc5ea8b12594f
troxika <dimaoparin95@gmail.com>
2026-01-27 18:44:47 +0300

    Update winsockserver.cpp

--- winsockserver.cpp
+++ winsockserver.cpp
@@ -1,162 +1,136 @@
-#undef UNICODE
-
-#define WIN32_LEAN_AND_MEAN
-
-#include <windows.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <stdlib.h>
-#include <iostream>
-#include <string>
-#include <fstream>
-// Need to link with Ws2_32.lib
-#pragma comment (lib, "Ws2_32.lib")
-// #pragma comment (lib, "Mswsock.lib")
-
-#define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT "27015"
-class safe {
-    std::string filename;
-    std::string message;
-    safe(std::string filenam, std::string message) {
-        filename = filenam;
-        message = message;
-    }
-    void insertfile(){
-        std::ofstream out2;
-        out2.open(filename, std::ios::app);\
-            out2 << message;
-    }
-    std::string readfile() {
-        std::ifstream in;
-        in.open(filename);
-        std::string buffer;
-        in >> buffer;
-        return buffer;
-    }
-};
-class forward
+using System.Net;
+using System.Net.Sockets;
+ 
+ServerObject server = new ServerObject();// создаем сервер
+await server.ListenAsync(); // запускаем сервер
+ 
+class ServerObject
 {
-    std::string message;
-};
-int main()
-{
-   
-    WSADATA wsaData;
-    int iResult;
-
-    SOCKET ListenSocket = INVALID_SOCKET;
-    SOCKET ClientSocket = INVALID_SOCKET;
-
-    struct addrinfo* result = NULL;
-    struct addrinfo hints;
-
-    int iSendResult;
-    char recvbuf[DEFAULT_BUFLEN];
-    int recvbuflen = DEFAULT_BUFLEN;
-
-    // Initialize Winsock
-    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (iResult != 0) {
-        std::cout << "WSAStartup failed with error:" << iResult << std::endl;
-        return 1;
+    TcpListener tcpListener = new TcpListener(IPAddress.Any, 27015); // сервер для прослушивания
+    List<ClientObject> clients = new List<ClientObject>(); // все подключения
+    protected internal void RemoveConnection(string id)
+    {
+        // получаем по id закрытое подключение
+        ClientObject? client = clients.FirstOrDefault(c => c.Id == id);
+        // и удаляем его из списка подключений
+        if (client != null) clients.Remove(client);
+        client?.Close();
     }
-
-    ZeroMemory(&hints, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
-    hints.ai_flags = AI_PASSIVE;
-
-    // Resolve the server address and port
-    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
-    if (iResult != 0) {
-        std::cout << "getaddrinfo failed with error: " << iResult << std::endl;
-        WSACleanup();
-        return 1;
-    }
-
-    // Create a SOCKET for the server to listen for client connections.
-    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-    if (ListenSocket == INVALID_SOCKET) {
-        std::cout << "socket failed with error: " << WSAGetLastError() << "\n";
-        freeaddrinfo(result);
-        WSACleanup();
-        return 1;
-    }
-
-    // Setup the TCP listening socket
-    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
-    if (iResult == SOCKET_ERROR) {
-        std::cout << "bind failed with error: " << WSAGetLastError() << "\n";
-        freeaddrinfo(result);
-        closesocket(ListenSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    freeaddrinfo(result);
-
-    iResult = listen(ListenSocket, SOMAXCONN);
-    if (iResult == SOCKET_ERROR) {
-        std::cout << "listen failed with error:" << WSAGetLastError() << "\n";
-        closesocket(ListenSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    // Accept a client socket
-    ClientSocket = accept(ListenSocket, NULL, NULL);
-    if (ClientSocket == INVALID_SOCKET) {
-        std::cout << "accept failed with error: " << WSAGetLastError() << "\n";
-        closesocket(ListenSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    // No longer need server socket
-    closesocket(ListenSocket);
-
-    // Receive until the peer shuts down the connection
-    do {
-
-        iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
-        if (iResult > 0) {
-            std::cout << "Bytes received: " << iResult << "\n";
-
-            // Echo the buffer back to the sender
-            iSendResult = send(ClientSocket, recvbuf, iResult, 0);
-            if (iSendResult == SOCKET_ERROR) {
-                std::cout << "send failed with error: " << WSAGetLastError() << "\n";
-                closesocket(ClientSocket);
-                WSACleanup();
-                return 1;
+    // прослушивание входящих подключений
+    protected internal async Task ListenAsync()
+    {
+        try
+        {
+            tcpListener.Start();
+            Console.WriteLine("Сервер запущен. Ожидание подключений...");
+ 
+            while (true)
+            {
+                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+ 
+                ClientObject clientObject = new ClientObject(tcpClient, this);
+                clients.Add(clientObject);
+                Task.Run(clientObject.ProcessAsync);
             }
-            std::cout << "Bytes sent: " <<  iSendResult << "\n";
         }
-        else if (iResult == 0)
-            std::cout << "Connection closing...\n";
-        else {
-            std::cout << "recv failed with error: " << WSAGetLastError() << "\n";
-            closesocket(ClientSocket);
-            WSACleanup();
-            return 1;
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
         }
-
-    } while (iResult > 0);
-
-    // shutdown the connection since we're done
-    iResult = shutdown(ClientSocket, SD_SEND);
-    if (iResult == SOCKET_ERROR) {
-        std::cout << "shutdown failed with error: " << WSAGetLastError() << "\n";
-        closesocket(ClientSocket);
-        WSACleanup();
-        return 1;
+        finally
+        {
+            Disconnect();
+        }
     }
-
-    // cleanup
-    closesocket(ClientSocket);
-    WSACleanup();
-
-    return 0;
+ 
+    // трансляция сообщения подключенным клиентам
+    protected internal async Task BroadcastMessageAsync(string message, string id)
+    {
+        foreach (var client in  clients)
+        {
+            if (client.Id != id) // если id клиента не равно id отправителя
+            {
+                await client.Writer.WriteLineAsync(message); //передача данных
+                await client.Writer.FlushAsync();
+            }
+        }
+    }
+    // отключение всех клиентов
+    protected internal void Disconnect()
+    {
+        foreach (var client in clients)
+        {
+            client.Close(); //отключение клиента
+        }
+        tcpListener.Stop(); //остановка сервера
+    }
+}
+class ClientObject
+{
+    protected internal string Id { get;} = Guid.NewGuid().ToString();
+    protected internal StreamWriter Writer { get;}
+    protected internal StreamReader Reader { get;}
+ 
+    TcpClient client;
+    ServerObject server; // объект сервера
+ 
+    public ClientObject(TcpClient tcpClient, ServerObject serverObject)
+    {
+        client = tcpClient;
+        server = serverObject;
+        // получаем NetworkStream для взаимодействия с сервером
+        var stream = client.GetStream();
+        // создаем StreamReader для чтения данных
+        Reader = new StreamReader(stream);
+        // создаем StreamWriter для отправки данных
+        Writer = new StreamWriter(stream);
+    }
+ 
+    public async Task ProcessAsync()
+    {
+        try
+        {
+            // получаем имя пользователя
+            string? userName = await Reader.ReadLineAsync();
+            string? message = $"{userName} вошел в чат";
+            // посылаем сообщение о входе в чат всем подключенным пользователям
+            await server.BroadcastMessageAsync(message, Id);
+            Console.WriteLine(message);
+            // в бесконечном цикле получаем сообщения от клиента
+            while (true)
+            {
+                try
+                {
+                    message = await Reader.ReadLineAsync();
+                    if (message == null) continue;
+                    message = $"{userName}: {message}";
+                    Console.WriteLine(message);
+                    await server.BroadcastMessageAsync(message, Id);
+                }
+                catch
+                {
+                    message = $"{userName} покинул чат";
+                    Console.WriteLine(message);
+                    await server.BroadcastMessageAsync(message, Id);
+                    break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            // в случае выхода из цикла закрываем ресурсы
+            server.RemoveConnection(Id);
+        }
+    }
+    // закрытие подключения
+    protected internal void Close()
+    {
+        Writer.Close();
+        Reader.Close();
+        client.Close();
+    }
 }
